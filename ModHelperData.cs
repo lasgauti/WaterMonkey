d2cf14b31407bff934c5f3f520ad56a26703b865
lasgauti <157837206+lasgauti@users.noreply.github.com>
2024-02-23 18:50:27 -0500

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,12 +2,12 @@ namespace WaterMonkey;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "40.0";
+    public const string WorksOnVersion = "41.0";
     public const string Version = "1.0.0";
     public const string Name = "WaterMonkey";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Water";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "lasgauti"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "WaterMonkey"; // TODO add your repo name here, also in the download url in README.md
 }
